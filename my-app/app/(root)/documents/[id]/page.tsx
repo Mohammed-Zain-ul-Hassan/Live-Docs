58ce45c4a2d4a1b663321dc3365db30d6c19fbec
Mohammed Zain ul Hassan <powehi29@gmail.com>
2024-07-28 17:41:31 +0500

    Added Document Title Change Feature

--- my-app/app/(root)/documents/[id]/page.tsx
+++ my-app/app/(root)/documents/[id]/page.tsx
@@ -1,16 +1,32 @@
 import CollaborativeRoom from '@/components/CollaborativeRoom'
 import { Editor } from '@/components/editor/Editor'
 import Header from '@/components/Header'
+import { getDocument } from '@/lib/actions/room.action'
 import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/nextjs'
 import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 const Document = async ({params: {id}}: SearchParamProps) => {
   const clerkUser = await currentUser();
+  if(!clerkUser) redirect('/sign-in');
+
+  const room = await getDocument({
+    roomId: id,
+    userId: clerkUser.emailAddresses[0].emailAddress,
+  })
+
+  if(!room) redirect('/')
+
+  //TODO : Assess the permissions of the user to access the document
 
   return (
     <main className='flex w-full flex-col items-center'>
-      <CollaborativeRoom/>
+      <CollaborativeRoom
+        roomId={id}
+        roomMetadata={room.metadata}
+        
+      />
     </main>
   )
 }
--- my-app/lib/actions/room.action.ts
+++ my-app/lib/actions/room.action.ts
@@ -37,13 +37,30 @@ export const createDocument = async ({userId, email}: CreateDocumentParams)=>{
 export const getDocument = async ({roomId, userId}:{roomId: string; userId: string}) => {
     try {
         const room = await liveblocks.getRoom(roomId);
+        //TODO: Bring back the access check
+        // const hasAccess = Object.keys(room.usersAccesses).includes(userId);
 
-        const hasAccess = Object.keys(room.usersAccesses).includes(userId);
-
-        if(!hasAccess) throw new Error('You do not have access to this document');
+        // if(!hasAccess) throw new Error('You do not have access to this document');
 
         return parseStringify(room);
     } catch (error) {
         console.log("Error Accessing the Document : ", error)
     }
 }
+
+export const updateDocument = async (roomId: string, title: string) => {
+    try {
+        const updatedRoom = await liveblocks.updateRoom(roomId, {
+            metadata:{
+                title
+            }
+        })
+
+        revalidatePath(`/documents/${roomId}`);
+
+        return parseStringify(updatedRoom);
+    } catch (error) {
+        console.log("Error updating the document : ", error);
+    }
+}
+
