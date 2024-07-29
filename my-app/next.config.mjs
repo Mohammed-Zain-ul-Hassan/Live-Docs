c85ebc991d50c881ebdd73643366069276cb9e89
Mohammed Zain ul Hassan <powehi29@gmail.com>
2024-07-29 23:53:50 +0500

    made it a pwa

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,5 +1,4 @@
-import { withSentryConfig } from '@sentry/nextjs';
-import withPWA from 'next-pwa';
+import {withSentryConfig} from '@sentry/nextjs';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -8,51 +7,47 @@ const nextConfig = {
   },
   images: {
     remotePatterns: [{ protocol: 'https', hostname: 'img.clerk.com' }]
-  }
+  },
+  manifest: {
+    theme_color: '#f69435',
+    background_color: '#f69435',
+    display: 'standalone',
+    scope: '/',
+    start_url: '/',
+    name: 'LiveDocs',
+    short_name: 'LiveDocs',
+    description: 'Your go-to collaborative documentation app',
+    icons: [
+      {
+        src: '/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/icon-256x256.png',
+        sizes: '256x256',
+        type: 'image/png',
+      },
+      {
+        src: '/icon-384x384.png',
+        sizes: '384x384',
+        type: 'image/png',
+      },
+      {
+        src: '/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  },
 };
 
-const pwaConfig = withPWA({
-  pwa: {
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-  },
-});
-
-const sentryConfig = withSentryConfig(nextConfig, {
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options
-
-  org: 'powehi',
+export default withSentryConfig(nextConfig, {
+  org: 'jsm-x9',
   project: 'javascript-nextjs',
-
-  // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
-
-  // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
-  // tunnelRoute: "/monitoring",
-
-  // Hides source maps from generated client bundles
   hideSourceMaps: true,
-
-  // Automatically tree-shake Sentry logger statements to reduce bundle size
   disableLogger: true,
-
-  // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-  // See the following for more information:
-  // https://docs.sentry.io/product/crons/
-  // https://vercel.com/docs/cron-jobs
   automaticVercelMonitors: true,
 });
-
-// Combine configurations
-export default pwaConfig(sentryConfig);
