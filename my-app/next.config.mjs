cf2b547207f698802135e4404624b978236a7bbb
Mohammed Zain ul Hassan <powehi29@gmail.com>
2024-07-29 14:06:31 +0500

    Update next.config.mjs

--- my-app/next.config.mjs
+++ my-app/next.config.mjs
@@ -1,6 +1,9 @@
 import {withSentryConfig} from '@sentry/nextjs';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  typescript:{
+    ignoreBuildErrors: true,
+  },
   images:{
     remotePatterns: [{protocol: 'https', hostname: 'img.clerk.com'}]
   }
